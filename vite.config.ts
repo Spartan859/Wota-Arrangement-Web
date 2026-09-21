20ec2bef81587942594ac42810d100595a813b44
Xiangyu Li <dijkstra0x3@gmail.com>
2026-09-21 23:54:21 +0800

    fix: 预优化Excel导出依赖

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  optimizeDeps: { include: ["exceljs"] },
   test: { include: ["tests/**/*.test.ts"], environment: "node" },
   build: { chunkSizeWarningLimit: 1000 },
 });
